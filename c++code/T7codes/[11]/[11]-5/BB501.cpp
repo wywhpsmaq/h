34259a52eae78ca89d8c590a2f845d3e6ed92199
wywhpsmaq <2290537581@qq.com>
2025-05-09 22:12:38 +0800

    In the code submitted 2025/5/9, some contents were added.

--- c++code/T7codes/[11]/[11]-5/BB501.cpp
+++ c++code/T7codes/[11]/[11]-5/BB501.cpp
@@ -1,13 +1,59 @@
 #include <bits/stdc++.h>
 using namespace std;
+int n;
+int p[999][999];
+bool v[999][999];
+int dx[4] = {-1, 1, 0, 0};
+int dy[4] = {0, 0, -1, 1};
 int main() {
-    int n;
-    vector<vector<int>> a(n, vector<int>(n));
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            cin >> a[i][j];
-        } 
+    cin >> n;
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < n; ++j) {
+            cin >> p[i][j];
+        }
     }
-    queue<int> q;
-
+    queue<pair<int, int>> q;
+    for (int i = 0; i < n; ++i) {
+        if (p[i][0] == 0) {
+            q.push({i, 0});
+            v[i][0] = true;
+        }
+        if (p[i][n - 1] == 0) {
+            q.push({i, n - 1});
+            v[i][n - 1] = true;
+        }
+    }
+    for (int j = 0; j < n; ++j) {
+        if (p[0][j] == 0) {
+            q.push({0, j});
+            v[0][j] = true;
+        }
+        if (p[n - 1][j] == 0) {
+            q.push({n - 1, j});
+            v[n - 1][j] = true;
+        }
+    }
+    while (!q.empty()) {
+        pair a = q.front();
+        q.pop();
+        for (int d = 0; d < 4; ++d) {
+            int nx = a.first + dx[d], ny = a.second + dy[d];
+            if (nx >= 0 && nx < n && ny >= 0 && ny < n && !v[nx][ny] && p[nx][ny] == 0) {
+                v[nx][ny] = true;
+                q.push({nx, ny});
+            }
+        }
+    }
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < n; ++j) {
+            if (p[i][j] == 0 && !v[i][j]) p[i][j] = 2;
+        }
+    }
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < n; ++j) {
+            cout << p[i][j] << " ";
+        }
+        cout << '\n';
+    }
+    return 0;
 }
