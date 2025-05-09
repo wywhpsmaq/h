560849886b284ca04741bdb2f975d3607bf4bfea
wywhpsmaq <2290537581@qq.com>
2025-05-09 21:41:17 +0800

    In the code submitted 2025/5/8, some contents were added.

---
+++ c++code/T7codes/[11]/[11]-5/BB501.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    int n;
+    vector<vector<int>> a(n, vector<int>(n));
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            cin >> a[i][j];
+        } 
+    }
+    queue<int> q;
+    
+}
