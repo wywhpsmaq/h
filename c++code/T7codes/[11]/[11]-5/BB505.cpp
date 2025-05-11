700af15cd852b5d80f3b36df4105bc01733bce19
wywhpsmaq <2290537581@qq.com>
2025-05-11 13:05:49 +0800

    In the code submitted 2025/5/11, some contents were added.

--- c++code/T7codes/[11]/[11]-5/BB505.cpp
+++ c++code/T7codes/[11]/[11]-5/BB505.cpp
@@ -1,30 +1,60 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-int dx[4] = {-1, 1, 0, 0};
-int dy[4] = {0, 0, -1, 1};
-bool v[999][999];
 int main() {
+    struct uu {
+        int x, y;
+    };
     int n, m;
     cin >> n >> m;
-    vector<vector<int> > p(n, vector<int>(m));
-    for (int i = 1; i <= n; i++) {
-        for (int j = 1; j <= m; j++) {
-            cin >> p[i][j];
+    int s1, s2, e1, e2;
+    vector<vector<pair<int, pair<int, int>>>> g(n, vector<pair<int, pair<int, int>>>(m));
+    vector<vector<uu>> p(n, vector<uu>(m, {-1, -1}));
+    vector<vector<bool>> v(n, vector<bool>(m, false));
+    vector<vector<int>> ma(n, vector<int>(m));
+    vector<uu> pa;
+    queue<uu> q;
+    int dx[4] = {1, 0, -1, 0};
+    int dy[4] = {0, 1, 0, -1};
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            cin >> ma[i][j];
         }
     }
-    int sx, sy, ex, ey;
-    cin >> sx >> sy >> ex >> ey;
-    queue<pair<int, int> > q;
-    q.push({sx,sy});
+    cin >> s1 >> s2 >> e1 >> e2;
+    s1--, s2--, e1--, e2--;
+    q.push({s1, s2});
+    v[s1][s2] = true;
+    bool f = false;
     while (!q.empty()) {
-        pair<int, int> a = q.front();
-        int x = a.first, y = a.second;
+        uu c = q.front();
         q.pop();
-        for (int i = 0; i < 4; i++) {
-            int nx = x + dx[i];
-            int ny = y + dy[i];
-            if (nx < 0 || nx >= n || ny < 0 || ny >= m || p[nx][ny] == 1 || v[nx][ny]) continue;
-            q.push({nx, ny});
+        if (c.x == e1 && c.y == e2) {
+            f = true;
+            break;
+        }
+        for (int d = 0; d < 4; d++) {
+            int nx = c.x + dx[d], ny = c.y + dy[d];
+            if (nx >= 0 && nx < n && ny >= 0 && ny < m && !v[nx][ny] && ma[nx][ny] == 0) {
+                v[nx][ny] = true;
+                p[nx][ny] = c;
+                q.push({nx, ny});
+            }
         }
     }
+    if (!f) {
+        cout << "no way";
+        return 0;
+    }
+    uu c = {e1, e2};
+    while (!(c.x == s1 && c.y == s2)) {
+        pa.push_back(c);
+        c = p[c.x][c.y];
+    }
+    pa.push_back({s1, s2});
+    reverse(pa.begin(), pa.end());
+    for (int i = 0; i < pa.size(); i++) {
+        cout << "(" << pa[i].x + 1 << "," << pa[i].y + 1 << ")";
+        if (i != pa.size() - 1) cout << "->";
+    }
+    return 0;
 }
--- c++code/T7codes/[11]/[11]-5/BB509.cpp
+++ c++code/T7codes/[11]/[11]-5/BB509.cpp
@@ -1,6 +1,56 @@
 #include <bits/stdc++.h>
 using namespace std;
-int main()
-{
-    
+int main() {
+    int n, m, a;
+    cin >> n >> m >> a;
+    vector<vector<int>> p(m, vector<int>(n, 0));
+    vector<vector<bool>> v(m, vector<bool>(n, false));
+    vector<vector<int>> d(m, vector<int>(n, -1));
+    queue<pair<int, int>> q;
+    int sx = -1, sy = -1, ex = -1, ey = -1;
+    int dx[4] = {1, 0, -1, 0};
+    int dy[4] = {0, 1, 0, -1};
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            char c;
+            cin >> c;
+            if (c == '#') {
+                p[i][j] = 0;
+            } else if (c == '.') {
+                p[i][j] = 1;
+            } else if (c == 'S') {
+                p[i][j] = 1;
+                sx = i;
+                sy = j;
+            } else if (c == 'E') {
+                p[i][j] = 1;
+                ex = i;
+                ey = j;
+            }
+        }
+    }
+    q.push({sx, sy});
+    v[sx][sy] = true;
+    d[sx][sy] = 0;
+    while (!q.empty()) {
+        auto [x, y] = q.front();
+        q.pop();
+        if (x == ex && y == ey) {
+            break;
+        }
+        for (int i = 0; i < 4; i++) {
+            int nx = x + dx[i], ny = y + dy[i];
+            if (nx >= 0 && nx < m && ny >= 0 && ny < n && p[nx][ny] == 1 && !v[nx][ny]) {
+                v[nx][ny] = true;
+                d[nx][ny] = d[x][y] + 1;
+                q.push({nx, ny});
+            }
+        }
+    }
+    if (d[ex][ey] == -1) {
+        cout << -1;
+    } else {
+        cout << a * d[ex][ey];
+    }
+    return 0;
 }
