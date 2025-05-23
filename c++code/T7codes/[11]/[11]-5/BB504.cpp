f3bd46167c2656c86e4dda8d2e3002b3a57afba6
wywhpsmaq <2290537581@qq.com>
2025-05-23 22:11:28 +0800

    In the code submitted 2025/5/23, some contents were added.

--- c++code/T7codes/[11]/[11]-5/BB504.cpp
+++ c++code/T7codes/[11]/[11]-5/BB504.cpp
@@ -22,7 +22,7 @@ int bfs() {
         }
         for (int i = 0; i < 4; i++) {
             int nx = x + dx[i], ny = y + dy[i];
-            if (nx<1 || ny<1 || nx>n || ny>n || p[nx][ny]) continue;
+            if (nx < 1 || ny < 1 || nx > n || ny > n || p[nx][ny]) continue;
             int nt = t + (d != -1 && i != d);
             if (nt < vis[nx][ny][i]) {
                 vis[nx][ny][i] = nt;
@@ -36,10 +36,14 @@ int main() {
     cin >> n;
     for (int i = 1; i <= n; i++) {
         for (int j = 1; j <= n; j++) {
-            char c; cin >> c;
-            if (c == 'A') sx = i, sy = j;
-            else if (c == 'B') ex = i, ey = j;
-            else if (c == 'x') p[i][j] = 1;
+            char c;
+            cin >> c;
+            if (c == 'A')
+                sx = i, sy = j;
+            else if (c == 'B')
+                ex = i, ey = j;
+            else if (c == 'x')
+                p[i][j] = 1;
         }
     }
     memset(vis, 0x3f3f3f3f, sizeof(vis));
