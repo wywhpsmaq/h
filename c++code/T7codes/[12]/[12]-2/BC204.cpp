d47c83e4ce02e17d96cf14128006aae5e25f8df8
wywhpsmaq <2290537581@qq.com>
2025-05-31 13:42:47 +0800

    In the code submitted 2025/5/31, some contents were added.

--- c++code/T7codes/[12]/[12]-2/BC204.cpp
+++ c++code/T7codes/[12]/[12]-2/BC204.cpp
@@ -1,55 +1,101 @@
 #include <bits/stdc++.h>
 using namespace std;
-vector<pair<int, int>> d;
-vector<int> p[114514];
-vector<int> v_;
-vector<bool> v;
-vector<int> sum;
-int n;
-int ml, mj;
-void dfs(int a, int num) {
-    v[a] = true;
-    if (num > ml) {
-        ml = num, mj = a;
-        v_ = sum;
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    int n;
+    cin >> n;
+    vector<int> db(n + 1, -1), bd(n + 1, 0), rn(n + 1, 0);
+    vector<int> d1(n + 1, -1), d2(n + 1, -1);
+    vector<int> p1(n + 1, -1), p3;
+    vector<bool> o(n + 1, false);
+    vector<vector<int>> p(n + 1);
+    vector<int> v(n + 1, 0);
+    queue<int> q, q1;
+    for (int i = 1; i < n; i++) {
+        int a, b, c;
+        cin >> a >> b >> c;
+        p[a].push_back(b);
+        p[b].push_back(a);
+        v[i + 1] = c;
     }
-    for (auto x : p[a]) {
-        if (!v[x]) {
-            sum.push_back(x);
-            dfs(x, num + 1);
-            sum.pop_back();
+    d1[1] = 0;
+    q.push(1);
+    int a = 1;
+    while (!q.empty()) {
+        int u = q.front();
+        q.pop();
+        if (d1[u] > d1[a]) a = u;
+        for (int v : p[u]) {
+            if (d1[v] == -1) {
+                d1[v] = d1[u] + 1;
+                q.push(v);
+            }
         }
     }
-}
-int m666() {
-    for (auto x : v_) {
-        d[x].second++;
+    d2[a] = 0, p1[a] = -1;
+    q1.push(a);
+    int b = a;
+    while (!q1.empty()) {
+        int u = q1.front();
+        q1.pop();
+        if (d2[u] > d2[b]) b = u;
+        for (int v : p[u]) {
+            if (d2[v] == -1) {
+                d2[v] = d2[u] + 1;
+                p1[v] = u;
+                q1.push(v);
+            }
+        }
     }
-    return 0;
-}
-int main() {
-    cin >> n;
-    d.push_back({0, 0});
-    d.push_back({0, 0});
-    for (int i = 1; i < n; i++) {
-        int x, y, z;
-        cin >> x >> y >> z;
-        p[x].push_back(y);
-        p[y].push_back(x);
-        d.push_back({z, 0});
+    int ml = d2[b], num = b;
+    while (num != -1) {
+        p3.push_back(num);
+        num = p1[num];
     }
-    d.push_back({0, 0});
-    for (int i = 2; i <= n; ++i) {
-        sum.resize(0);
-        v.assign(n + 10, 0);
-        sum.push_back(i);
-        dfs(i, 0);
-        m666();
+    reverse(p3.begin(), p3.end());
+    for (int u : p3) o[u] = true;
+    for (int u : p3) {
+        db[u] = 0;
+        rn[u] = u;
+        q.push(u);
     }
-    int ans = 0;
-    for (int i = 1; i <= n; i++) {
-        if (d[i].second == n - 1) {
-            ans += d[i].first;
+    while (!q.empty()) {
+        int u = q.front();
+        q.pop();
+        for (int v : p[u]) {
+            if (db[v] == -1) {
+                db[v] = db[u] + 1;
+                rn[v] = rn[u];
+                if (db[v] > bd[rn[u]]) {
+                    bd[rn[u]] = db[v];
+                }
+                q.push(v);
+            }
+        }
+    }
+    int l = 0;
+    for (int i = 0; i < p3.size(); i++) {
+        if (bd[p3[i]] == i){
+            l = i;
+        }
+        else{
+            break;
+        }
+    }
+    int r = p3.size() - 1;
+    for (int i = p3.size() - 1; i >= 0; i--) {
+        if (bd[p3[i]] == (p3.size() - 1 - i)){
+            r = i;
+        }
+        else{
+            break;
+        }
+    }
+    long long ans = 0;
+    if (l <= r) {
+        for (int i = l; i <= r; i++) {
+            ans += v[p3[i]];
         }
     }
     cout << ans;
