2bd652d742f944a2ae003c7589d67494ea5c1976
wywhpsmaq <2290537581@qq.com>
2025-07-05 22:28:40 +0800

    In the code submitted 2025/7/5, some contents were added.

--- c++code/cs.cpp
+++ c++code/cs.cpp
@@ -1,16 +1,50 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-int main()
-{
-    int ans=0,ans_=0;
-    srand(chrono::steady_clock::now().time_since_epoch().count());
-    for(int i=0;i<1000000;i++)
-    {
-        int x=rand()%2;
-        if(x==0)
-            ans++;
-        else
-            ans_++;
+vector<pair<int,int>> g[114514];
+int n, m;
+int dis[114514], cnt[114514];
+bool vi[114514];
+bool f() {
+    fill(dis, dis + n + 2, 0);
+    fill(cnt, cnt + n + 2, 0);
+    fill(vi, vi + n + 2, false);
+    queue<int> q;
+    for (int i = 0; i <= n; ++i) {
+        q.push(i);
+        vi[i] = true;
     }
-    cout<<(ans>ans_?"Yes!":"No!")<<endl;
+    while (!q.empty()) {
+        int u = q.front(); q.pop();
+        vi[u] = false;
+        for (auto &e : g[u]) {
+            int v = e.first, w = e.second;
+            if (dis[v] > dis[u] + w) {
+                dis[v] = dis[u] + w;
+                cnt[v] = cnt[u] + 1;
+                if (cnt[v] > n + 1) return false;
+                if (!vi[v]) {
+                    q.push(v);
+                    vi[v] = true;
+                }
+            }
+        }
+    }
+    return true;
+}
+int main() {
+    int w;
+    cin >> w;
+    while (w--) {
+        cin >> n >> m;
+        for (int i = 0; i <= n; ++i) g[i].clear();
+        for (int i = 0; i < m; ++i) {
+            int l, r, v;
+            cin >> l >> r >> v;
+            g[l - 1].push_back({r, v});
+            g[r].push_back({l - 1, -v});
+        }
+        if (f()) cout << "true\n";
+        else cout << "false\n";
+    }
+    return 0;
 }
