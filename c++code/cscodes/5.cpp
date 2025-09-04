e720721196f85b7cbd9f9a3bf44d9b0099b36f2f
wywhpsmaq <2290537581@qq.com>
2025-09-04 21:37:36 +0800

    In the code submitted 2025/9/3, some contents were added.

--- c++code/cscodes/5.cpp
+++ c++code/cscodes/5.cpp
@@ -1,33 +1,18 @@
 #include <bits/stdc++.h>
+#include <windows.h> // 用于设置控制台编码
 using namespace std;
+
 int main () {
-	int n, m;
-	cin >> n >> m;
-	vector<int> g[114];
-	for (int i = 0; i < m; i++) {
-		int x, y;
-		cin >> x >> y;
-		g[x].push_back (y);
-		g[y].push_back (x);
-	}
-	int a;
-	cin >> a;
-	vector<int> v;
-	for (int i = 0; i < a; i++) {
-		int x;
-		cin >> x;
-		v.push_back (x);
-	}
-	vector<int> p;
-	for (int i = 1; i < a; i++) { p[i] = p[i - 1] + v[i]; }
-	int i = 0;
-	int num = 1;
-	while (num) {
-		num += p[i];
-		num /= v[i];
-		i++;
-		i %= a;
-	}
-	cout << num << " " << p[a - 1] - v[a - 1];
+	freopen("1.txt","r",stdin);
+	freopen("2.txt","w",stdout);
+	// 设置控制台输入输出编码为UTF-8
+	SetConsoleOutputCP (65001); // 输出编码
+	SetConsoleCP (65001);		// 输入编码
+
+	string chinese;
+	cout << "请输入中文：";
+	getline (cin, chinese);
+	cout << "你输入的中文是：" << chinese << endl;
+
 	return 0;
 }
